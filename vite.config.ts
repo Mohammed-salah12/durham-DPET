6d88a20c3c8b2393bae581a965f838d39c9c762f
Mohammed-salah12 <mohammedsalah899000@gmail.com>
2026-01-22 15:53:06 +0200

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@
   import path from 'path';
 
   export default defineConfig({
-    plugins: [react()],
+  base: '/durham-DPET/',
+  plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
